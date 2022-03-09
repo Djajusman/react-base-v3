1be30904bfc998cf34bac16fcb53c514d4867db7
Djajusman <tsaniyadiriyadh@gmail.com>
2022-03-09 23:28:13 +0700

    add FCM

--- src/config/initFirebase.js
+++ src/config/initFirebase.js
@@ -1,44 +1,42 @@
-import firebase from "firebase";
-import 'firebase/messaging';
+import { useEffect } from 'react';
+import { initializeApp } from "firebase/app";
+import { getMessaging, getToken, onMessage } from 'firebase/messaging';
 
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-    apiKey: "AIzaSyB4Rew6VfT94NhjCCY4TgATu9lcSKuDp48",
-    authDomain: "test-ddac5.firebaseapp.com",
-    projectId: "test-ddac5",
-    storageBucket: "test-ddac5.appspot.com",
-    messagingSenderId: "107081019969",
-    appId: "1:107081019969:web:02ba90b203f36dacfc8cc0",
-    measurementId: "G-F9VS4W64W8"
+export default function FirebaseMessaging() {
+  const firebaseConfig = {
+    apiKey: "AIzaSyD-_n3iKZcMor_j1jpOqGyRfqpH16Yu8QU",
+    authDomain: "fcm-test-ea1ca.firebaseapp.com",
+    projectId: "fcm-test-ea1ca",
+    storageBucket: "fcm-test-ea1ca.appspot.com",
+    messagingSenderId: "592535260625",
+    appId: "1:592535260625:web:b666af36c0a477d8cfa4f0"
   };
 
-firebase.initializeApp(firebaseConfig);
-export const firebaseDatabase = firebase.database();
-export const firebaseAuth = firebase.auth();
-export const firebaseStorage = firebase.storage();
-const messaging = firebase.messaging();
-export const onMessageListener = () =>
-  new Promise((resolve) => {
-    messaging.onMessage((payload) => {
-      resolve(payload);
+  // Initialize Firebase
+  useEffect(() => {
+    initializeApp(firebaseConfig);
+    const messaging = getMessaging();
+    getToken(messaging, { vapidKey: 'BLhTePlz2N76Zv0UvyWX9Pw5Hv5z7837gkbDpeI7JsgAhY4rIyjDZMl7P4nkE3upavhYOs-8-9Xtvpfk0i6Rm7g' }).then((currentToken) => {
+      if (currentToken) {
+        console.log("firebase token : ", currentToken )
+        // Send the token to your server and update the UI if necessary
+        // ...
+      } else {
+        // Show permission request UI
+        console.log('No registration token available. Request permission to generate one.');
+        // ...
+      }
+    }).catch((err) => {
+      console.log('An error occurred while retrieving token. ', err);
+      // ...
     });
-});
-export const getToken = (setTokenFound) => {
-  return messaging.getToken({vapidKey: 'GENERATED_MESSAGING_KEY'}).then((currentToken) => {
-    if (currentToken) {
-      console.log('current token for client: ', currentToken);
-      setTokenFound(true);
-      // Track the token -> client mapping, by sending to backend server
-      // show on the UI that permission is secured
-    } else {
-      console.log('No registration token available. Request permission to generate one.');
-      setTokenFound(false);
-      // shows on the UI that permission is required 
-    }
-  }).catch((err) => {
-    console.log('An error occurred while retrieving token. ', err);
-    // catch error while creating client token
-  });
+    onMessage(messaging, (payload) => {
+      console.log('Message received. ', payload);
+      // ...
+    });
+  })
+  return (
+    <></>
+  )
 }
 
-export default firebase;
